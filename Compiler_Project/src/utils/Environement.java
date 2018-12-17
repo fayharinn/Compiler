fe1c1af84d4de2c2464e4aa9a55a54a6f2c545c2
Mouataz <mouataz.amal@gmail.com>
2018-12-17 18:47:51 +0100

    Méthode du premier accept ajouté dans le type checkVisitor

---
+++ Compiler_Project/src/utils/Environement.java
@@ -0,0 +1,39 @@
+package utils;
+
+import type.Type;
+
+import java.util.HashMap;
+
+public class Environement {
+    private HashMap<String, Type> gho;
+
+
+    public Environement() {
+        this.gho = new HashMap<String, Type>();
+    }
+
+    public Environement(HashMap<String, Type> gho) {
+        this.gho = gho;
+    }
+
+    public HashMap<String, Type> getGho() {
+        return gho;
+    }
+
+    public void setGho(HashMap<String, Type> gho) {
+        this.gho = gho;
+    }
+
+    public void ajouterVar(String id, Type t){
+        this.gho.put(id,t);
+    }
+
+    public void removeVar(String id){
+        gho.remove(id);
+    }
+
+    public Type getTypeofVar(String id){
+        return gho.get(id);
+    }
+
+}
