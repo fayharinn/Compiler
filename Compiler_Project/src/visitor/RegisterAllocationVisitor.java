799b26f8a8b5dbf9c88ec5304763aa71977ce0d3
Alice JACQUIER <alicejacquier.17@gmail.com>
2019-01-08 19:18:39 +0100

    Debut Var

--- Compiler_Project/src/visitor/RegisterAllocationVisitor.java
+++ Compiler_Project/src/visitor/RegisterAllocationVisitor.java
@@ -2,6 +2,7 @@ package visitor;
 
 import ast.*;
 import ast.Float;
+import type.Type;
 import utils.Id;
 
 import java.util.ArrayList;
@@ -9,10 +10,16 @@ import java.util.HashMap;
 
 public class RegisterAllocationVisitor implements ObjVisitor<Exp>  {
 
-    private HashMap<Id, Integer> index; // Liste des variables associées aux registres. Si negatif alors offset en memoire.
+    private HashMap<Id, Integer> indexRegistres; // Liste des variables associées aux registres.
+    private HashMap<Id, Integer> indexStack; // Liste des variables associées à leur offset dans la pile.
+
+    private int getRegistre(){
+        return 0;
+    }
 
     public RegisterAllocationVisitor(){
-        index = new HashMap<Id, Integer>();
+        indexRegistres = new HashMap<Id, Integer>();
+        indexStack = new HashMap<Id, Integer>();
     }
 
     public Exp visit(Unit e) {
@@ -32,102 +39,89 @@ public class RegisterAllocationVisitor implements ObjVisitor<Exp>  {
     }
     
     public Exp visit(Not e) {
-        return e;
+        return new Not(e.e.accept(this));
     }
     
     public Exp visit(Neg e) {
-        return e;
+        return new Neg(e.e.accept(this));
     }
     
     public Exp visit(Add e) {
         return null;
     }
-
     
     public Exp visit(Sub e) {
         return null;
     }
-
     
     public Exp visit(FNeg e) {
         return null;
     }
-
     
     public Exp visit(FAdd e) {
         return null;
     }
-
     
     public Exp visit(FSub e) {
         return null;
     }
-
     
     public Exp visit(FMul e) {
         return null;
     }
-
     
     public Exp visit(FDiv e) {
         return null;
     }
-
     
     public Exp visit(Eq e) {
         return null;
     }
-
     
     public Exp visit(LE e) {
         return null;
     }
-
     
     public Exp visit(If e) {
         return null;
     }
-
     
     public Exp visit(Let e) {
         return null;
     }
-
     
     public Exp visit(Var e) {
-        return null;
+        int registre = indexRegistres.get(e.id);
+        if(registre >= 0){ //La variable est dans un registre
+            return new Var(new Id("r" + registre));
+        }else{
+            return null;
+        }
     }
 
-    
     public Exp visit(LetRec e) {
         return null;
     }
-
     
     public Exp visit(App e) {
         return null;
     }
-
     
     public Exp visit(Tuple e) {
         return null;
     }
-
     
     public Exp visit(LetTuple e) {
         return null;
     }
-
     
     public Exp visit(Array e) {
         return null;
     }
-
     
     public Exp visit(Get e) {
         return null;
     }
-
     
     public Exp visit(Put e) {
         return null;
