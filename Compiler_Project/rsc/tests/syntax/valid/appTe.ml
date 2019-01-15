0107cd9ff3684020d14b28086712f235afbe865d
secardia <aurelien.secardin@icloud.com>
2019-01-15 15:45:33 +0100

    LA CEST PAS BON

---
+++ Compiler_Project/rsc/tests/syntax/valid/appTe.ml
@@ -0,0 +1,7 @@
+let rec sum x y =
+    let z = x+y in
+    z
+in
+let a = 1 in
+let b = 2 in
+print_int(sum a b)
--- Compiler_Project/rsc/tests/typecheck/valid/1fadd_float_float.ml
+++ Compiler_Project/rsc/tests/typecheck/valid/1fadd_float_float.ml
@@ -1,4 +1,4 @@
-let x = 2.5 in
-let y = 5.1 in
+let x = 5.2 in
+let y = 2.2 in
 let z = y+.x in
-z
+x
