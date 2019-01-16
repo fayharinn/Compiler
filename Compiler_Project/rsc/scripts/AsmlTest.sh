8650c0065ba4bfe567ab0de6b3f60de2dfc935a0
secardia <aurelien.secardin@icloud.com>
2019-01-16 12:36:26 +0100

    Pour debug

--- Compiler_Project/rsc/scripts/AsmlTest.sh
+++ Compiler_Project/rsc/scripts/AsmlTest.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 CPARG="-cp ../../java-cup-11b-runtime.jar:../../java-cup-11b.jar:../../out"
 
+
+i=0
 echo "================> ASML Tests"
 for test_case in ../tests/asml/*.ml
 do
     echo $test_case
-    java $CPARG AsmlTest $test_case $test_case+"_tmp.asml"
-    ./../tests/asml/asml $test_case+"_tmp.asml"
+    file=$i'_tmp.asml'
+    java $CPARG AsmlTest $test_case $file
+    ./../tests/asml/asml $file
+    i=`expr $i + 1`
 done
